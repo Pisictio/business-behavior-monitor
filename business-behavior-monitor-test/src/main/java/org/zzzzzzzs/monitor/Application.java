341e79108e3ed856a6c775380ad2c02d77ba47d2
Pisictio <zzzzzzzs11@qq.com>
2024-07-28 11:33:45 +0800

    feat: test init

---
+++ business-behavior-monitor-test/src/main/java/org/zzzzzzzs/monitor/Application.java
@@ -0,0 +1,20 @@
+package org.zzzzzzzs.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zzs
+ * @description 启动类
+ * @create 2024/7/28 10:32
+ */
+
+@SpringBootApplication
+@Configuration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ business-behavior-monitor-test/src/test/java/com/zzzzzzzs/test/ApiTest.java
@@ -0,0 +1,17 @@
+package com.zzzzzzzs.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author zzs
+ * @description
+ * @create 2024/7/28 11:22
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+}
